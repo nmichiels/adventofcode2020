9ae31fb887190c55654093f40c2d28000675b47a
Nick Michiels <michiels.nick@gmail.com>
2020-12-24 16:51:06 +0100

    solution with linked list and pointer array

--- day23/day23.py
+++ day23/day23.py
@@ -3,13 +3,13 @@ import time
 import re
 # https://adventofcode.com/2020/day/23
 
-cups = '389125467'
+cups = '215694783'
 cups = [int(cup) for cup in list(cups)]
 
-# cups = cups + list(range(10,1000001))
+cups = cups + list(range(10,1000001))
 num_cups = len(cups)
 print(num_cups)
-num_moves = 100
+num_moves = 10000000
 
 
 
@@ -26,14 +26,31 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+        self.tail = None
         
     def add_first(self, node):
         
         node.next = self.head
-        if self.head is not None:
-            self.head.prev = node
+        if node.next is not None:
+            node.next.prev = node
+        else:
+            self.tail = node
         self.head = node
-    
+        
+    def print_reversed(self):
+        node = self.tail
+        nodes = []
+        count = 0
+        while node is not None and (count == 0 or node != self.tail):
+            nodes.append(str(node.data))
+            node = node.prev
+            count += 1
+        if node == self.tail:
+            nodes.append("tail")
+        else:
+            nodes.append("None")
+        print(" -> ".join(nodes))
+        
     def __repr__(self):
         node = self.head
         nodes = []
@@ -51,24 +68,23 @@ class LinkedList:
 llist = LinkedList()
 
 
-last = None
+pointers = {}
 for i in reversed(range(len(cups))):
     node = Node(cups[i])
-    if i == (len(cups) -1):
-        last = node
     llist.add_first(node)
+    pointers[cups[i]] = node
+    
 
 # make circular
-last.next = llist.head
-llist.head.prev = last
+llist.tail.next = llist.head
+llist.head.prev = llist.tail
+
 
 
 start = time.time()
 current = llist.head
 for move in range(num_moves):
     
-    if move % 100 == 0:
-        print(move)
     # find destination
     steps = 0
     destination_val = current.data - 1
@@ -81,18 +97,24 @@ for move in range(num_moves):
             destination_val += num_cups
         
     # find destination node
-    destination = current.next.next.next.next
-    while destination.data != destination_val:
-        destination = destination.next
+    destination = pointers[destination_val]
+    # destination = current.prev
+    # while destination.data != destination_val:
+        # destination = destination.prev
     
-        
+
     # remove pickuped cups
     pickup = current.next
+    current.next.next.next.next.prev = current
     current.next = current.next.next.next.next
+    
     
     # move picked upped cups just after destination
     pickup.next.next.next = destination.next
+    destination.next.prev = pickup.next.next
+    
     destination.next = pickup
+    pickup.prev = destination
     
     # print(current.data, destination_val)
     # print(llist)
@@ -101,18 +123,15 @@ for move in range(num_moves):
 end = time.time()
 
 
-# set cup before the cup with value 1 as head node
-node = llist.head
-while node.next.data != 1:
-    node = node.next
-llist.head = node
-
 # get result part 1
 labels = []
-node = llist.head.next.next
+node = pointers[1].next
 while node.data != 1:
     labels.append(str(node.data))
     node = node.next
 print("Result part 1: ", "".join(labels))
-print("Result part 2: ", llist.head.data, '*', llist.head.next.next.data, "=", llist.head.data*llist.head.next.next.data)
+
+# print(pointers[4013].next.data)
+print(pointers[1].next.data, pointers[1].next.next.data)
+print("Result part 2: ", pointers[1].next.data, '*', pointers[1].next.next.data, "=", pointers[1].next.data*pointers[1].next.next.data)
 print(end-start)
